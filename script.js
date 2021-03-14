7c85a34f9ac34cefc76f71f3b0298bd07a007519
RReiso <ruta.littlemore@gmail.com>
2021-03-13 19:36:20 -0500

    Changed names of classes, deleted comments from script.js file

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const sideNav = document.querySelector(".nav");
-// const overlay = document.querySelector(".overlay");
 const ham = document.querySelector("#ham");
-// const menuX = document.querySelector(".menuX");
 const menuItems = document.querySelectorAll(".nav-link");
 
 menuItems.forEach((menuItem) => {
@@ -9,10 +7,7 @@ menuItems.forEach((menuItem) => {
 });
 
 ham.addEventListener("click", toggleHamburger);
-// menuX.addEventListener("click", toggleHamburger);
-// overlay.addEventListener("click", toggleHamburger);
 
 function toggleHamburger() {
-	// overlay.classList.toggle("showOverlay");
 	sideNav.classList.toggle("showNav");
 }
